870cb1cca275a0ba8701bcb885d96f9f7670555e
Data-Star <DataStar29@outlook.com>
2015-08-23 11:18:42 +0800

    rename folder and reload

---
+++ Exploratory Data Analysis - Peer Assignment 1/Readme.R
@@ -0,0 +1,23 @@
+#For the Coursera Course "Exploratory Data Analysis", Week 1 Peer Assignment 1.
+
+#For and clone the following GitHub repository:
+https://github.com/Data-Star/ExData_Plotting1
+
+#To run this script, please do the following:
+#Download the scripts (plot1.R, plot2.R, plot3.R, plot4.R) to a local directory
+
+#Download the data
+https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+#Measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+#The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
+#Date: Date in format dd/mm/yyyy
+#Time: time in format hh:mm:ss
+#Global_active_power: household global minute-averaged active power (in kilowatt)
+#Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+#Voltage: minute-averaged voltage (in volt)
+#Global_intensity: household global minute-averaged current intensity (in ampere)
+#Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+#Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+#Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
---
+++ Programming - Peer Assignment 2/cachematrix.R
@@ -0,0 +1,54 @@
+# this function creates a special "matrix" object, list 
+# contains 4 member functions: set, get, setInv and getInv.
+# it uses <<- assignment operator so that these internal
+# variables are not exposed to the outside environment.
+
+makeCacheMatrix <- function(x = matrix()) {
+  xinv <- NULL # this is where the result of inversion is stored
+  # A set of function, use this to set a matrix to object created by makeCacheMatrix function
+  # e.g makeCacheMatrix(testmatrix) # here we work on testmatrix
+  # makeCacheMatrix$set(testmatrix1) # here we work on testmatrix1
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL # it also initialises xinv to null
+  }
+  
+  get <- function() x # return the input matrix
+  setInv <- function(inv) xinv <<- inv # set the inversed matrix
+  getInv <- function() xinv # return the inversed matrix
+  # return a list that contains these functions, so that we can use
+  # makeCacheMatrix object like these
+  # x <- makeCacheMatrix(testmatrix)
+  # x$set(newmatrix) # to change matrix
+  # x$get # to get the setted matrix
+  # x$setInv # to set the inversed matrix
+  # x$getInv # to get the inversed matrix
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+}
+
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv() # get the inversed matrix from object x
+  # it will be null if uncalculated, remember the first line "xinv <- NULL" in the previous function
+  if(!is.null(m)) { # if the inversion result is there
+    message("getting cached data")
+    return(m) # return the calculated inversion
+  }
+  data <- x$get() # if not, we do x$get to get the matrix object
+  m <- solve(data) # we solve it
+  x$setInv(m) # we then set it to the object
+  m # return the solved result
+}
+# Test
+# generate a random square, non-singular matrix
+test <- matrix(runif(20,2,100),2,2)
+# generate the makeCacheMatrix object with this matrix
+testCached <- makeCacheMatrix(test)
+# from now on calculate or retrieve calculated inversion using the cacheSolve function
+
+testInv <- cacheSolve(testCached)
